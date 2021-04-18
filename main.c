1affa075088cba736fddf8224605159028484d14
mayar wael <mayar.abdelrahman00@eng-st.cu.edu.eg>
2021-04-19 00:18:56 +0200

    welcome to the structure

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+    char name[50];
+    int iD;
+    float marks;
+    int day;
+    int month;
+    int year;
+} s;
+
+int main()
+{
+    printf("welcome to the program!\n");
+    return 0;
+}
